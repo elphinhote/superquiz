e2b0c4b70c156cb614cece3f11527aba79b304ac
Eva Wang <elphinhote@yahoo.com>
2020-10-26 19:05:29 -0700

    testing new repo

---
+++ app.js
@@ -0,0 +1,156 @@
+
+(function(){
+  function buildQuiz(){
+    // variable to store the HTML output
+    const output = [];
+
+    // for each question...
+    myQuestions.forEach(
+      (currentQuestion, questionNumber) => {
+
+        // variable to store the list of possible answers
+        const answers = [];
+
+        // and for each available answer...
+        for(letter in currentQuestion.answers){
+
+          // ...add an HTML radio button
+          answers.push(
+            `<label>
+              <input type="radio" name="question${questionNumber}" value="${letter}">
+              ${letter} :
+              ${currentQuestion.answers[letter]}
+            </label>`
+          );
+        }
+
+        // add this question and its answers to the output
+        output.push(
+          `<div class="question"> ${currentQuestion.question} </div>
+          <div class="answers"> ${answers.join('')} </div>`
+        );
+      }
+    );
+
+    // combine the output list into one string of HTML and put it on the page
+    quizContainer.innerHTML = output.join('');
+  }
+
+  function showResults(){
+
+    // gather answer containers from our quiz
+    const answerContainers = quizContainer.querySelectorAll('.answers');
+
+    // keep track of user's answers
+    let numCorrect = 0;
+
+    // for each question...
+    myQuestions.forEach( (currentQuestion, questionNumber) => {
+
+      // find selected answer
+      const answerContainer = answerContainers[questionNumber];
+      const selector = `input[name=question${questionNumber}]:checked`;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+      // if answer is correct
+      if(userAnswer === currentQuestion.correctAnswer){
+        // add to the number of correct answers
+        numCorrect++;
+
+        // color the answers blue
+        answerContainers[questionNumber].style.color = 'lightblue';
+      }
+      // if answer is wrong or blank
+      else{
+        // color the answers red
+        answerContainers[questionNumber].style.color = 'red';
+      }
+    });
+
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+  }
+
+  const quizContainer = document.getElementById('quiz');
+  const resultsContainer = document.getElementById('results');
+  const submitButton = document.getElementById('submit');
+  const myQuestions = [
+    {
+      question: " 1. A very useful tool used during development and debugging for printing to the debugger is?",
+      answers: {
+        a: "Javascript",
+        b: "terminal/bash",
+        c: "for loops"
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: "2. String values must be enclosed within _____ when being assigned to variables.",
+      answers: {
+        a: "commas",
+        b: "curley brackets",
+        c: "quotes"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "3. Commonly used data types DO NOT include?",
+      answers: {
+        a: "strings",
+        b: "booleans",
+        c: "alerts"
+      },
+      correctAnswer: "c"
+    },
+    
+    {
+      question: "4. The condition in an if / else statement is enclosed within ____?",
+      answers: {
+        a: "quotes",
+        b: "curly brackets",
+        c: "parentheses"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "5. Arrays in Javascript can be used to store____?",
+      answers: {
+        a: "numbers and strings",
+        b: "other arrays",
+        c: "booleans",
+        d: "all of the above"
+      },
+      correctAnswer: "d"
+    }
+  ];
+
+  // Kick things off
+  buildQuiz();
+
+  // Event listeners
+  submitButton.addEventListener('click', showResults);
+})();
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = 0;
+            // timer = duration; // uncomment this line to reset timer automatically after reaching 0
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var time = 120 / 2, // time in seconds here
+        display = document.querySelector('#Timer');
+    startTimer(time, display);
+};
---
+++ readme.md
@@ -0,0 +1,96 @@
+04 Web APIs: Code Quiz
+
+
+ Task
+ 
+ 
+Develop a multiple-choice questions and interactive coding quiz.
+
+Build a timed coding quiz with multiple-choice questions. 
+
+The app will run in the browser, and will feature dynamically 
+updated HTML and CSS powered by JavaScript code that you write. 
+
+
+Acceptance Criteria
+GIVEN I am taking a code quiz
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+
+Mock-Up
+The following animation demonstrates the application functionality:
+![Quiz](https://user-images.githubusercontent.com/65749636/97097894-c2117800-1633-11eb-93db-e6d4abc7e254.PNG)
+
+
+Grading Requirements
+This homework is graded based on the following criteria:
+
+Technical Acceptance Criteria: 40%
+
+Satisfies all of the above acceptance criteria.
+
+
+Deployment: 32%
+
+
+Application deployed at live URL.
+
+
+Application loads with no errors.
+
+
+Application GitHub URL submitted.
+
+
+GitHub repository that contains application code.
+
+
+
+Application Quality: 15%
+
+
+Application user experience is intuitive and easy to navigate.
+
+
+Application user interface style is clean and polished.
+
+
+Application resembles the mock-up functionality provided in the homework instructions.
+
+
+
+Repository Quality: 13%
+
+
+Repository has a unique name.
+
+
+Repository follows best practices for file structure and naming conventions.
+
+
+Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
+
+
+Repository contains multiple descriptive commit messages.
+
+
+Repository contains quality README file with description, screenshot, and link to deployed application.
+
+
+
+Review
+You are required to submit BOTH of the following for review:
+
+
+The URL of the functional, deployed application.
+
+
+The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
+
+
